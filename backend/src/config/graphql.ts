7f39e18ce8f79a423ab5f884aaa72507fd66eaea
Adam Cardenas <cevaris@gmail.com>
2021-06-09 06:38:40 -0600

    working graphql get tasks + fields

--- backend/src/config/graphql.ts
+++ backend/src/config/graphql.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from 'apollo-server-express';
 import { GraphQLScalarType } from 'graphql';
 import 'graphql-import-node';
 import * as typeDefs from '../../../common/schema.graphql';
+import { firebaseDb } from '../db/firebaseClient';
 import { TasksDb } from '../db/tasksDb';
 
 // https://stackoverflow.com/a/66827537/3538289
@@ -11,18 +12,26 @@ const dateScalar = new GraphQLScalarType({
   parseValue(value) {
     return new Date(value);
   },
-  serialize(value) {
-    return value.toISOString();
+  serialize(value: Date | FirebaseFirestore.Timestamp) {
+    console.log('serializing date', value);
+    if ('toISOString' in value) {
+      return value.toDateString();
+    }
+    if ('toDate' in value) {
+      return value.toDate();
+    }
+    throw Error(`could not serialize date value ${value}`);
   },
 })
+
+const taskDB = new TasksDb(firebaseDb);
 
 const resolvers = {
   Date: dateScalar,
   Query: {
     hello: () => 'world2',
     getTasks: async () => {
-      // new TasksDb().get()
-      return await Promise.resolve([]);
+      return await taskDB.get()
     }
   },
 };
