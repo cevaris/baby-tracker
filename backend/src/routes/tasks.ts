f88044c4215a11c3dfac4d55d7e2ae9dac0a4f02
Adam Cardenas <cevaris@gmail.com>
2021-06-21 17:41:09 -0600

    cleanup

--- backend/src/routes/tasks.ts
+++ backend/src/routes/tasks.ts
@@ -5,21 +5,24 @@ import { ApiError, ApiTask } from '../types/api';
 
 const router = express.Router();
 
-const tasks: ApiTask[] = [
-    { title: 'Tummy time', description: 'Sit tummy down for at least 3 minutes.', id: '1', fields: [] },
-    { title: 'Feeding', description: 'Feed and burp.', id: '2', fields: [{ name: 'ml', description: 'How much milliliters?', type: 'number', is_required: true }] },
-    { title: 'Poop', description: 'Observed poop in pamper.', id: '3', fields: [] },
-    { title: 'Old task', description: 'This task is no longer.', id: '4', fields: [], disabled_at: new Date('04/05/2020').toISOString() },
-    { title: 'Non Required field', description: 'This field task value is not required.', id: '5', fields: [{ name: 'task_field1', description: 'optional field?', type: 'input', is_required: false }] },
-];
+// const tasks: ApiTask[] = [
+//     { title: 'Tummy time', description: 'Sit tummy down for at least 3 minutes.', id: '1', fields: [] },
+//     { title: 'Feeding', description: 'Feed and burp.', id: '2', fields: [{ name: 'ml', description: 'How much milliliters?', type: 'number', is_required: true }] },
+//     { title: 'Poop', description: 'Observed poop in pamper.', id: '3', fields: [] },
+//     { title: 'Old task', description: 'This task is no longer.', id: '4', fields: [], disabled_at: new Date('04/05/2020').toISOString() },
+//     { title: 'Non Required field', description: 'This field task value is not required.', id: '5', fields: [{ name: 'task_field1', description: 'optional field?', type: 'input', is_required: false }] },
+// ];
 
-router.get('/tasks.json', async (req: express.Request, res: express.Response<ApiTask[]>) => {
-    const tasks = await TasksDb.getTasks();
-    res.json(tasks.map(presentTask));
+router.get('/tasks.json', async (req: express.Request, res: express.Response<ApiTask[] | ApiError>) => {
+    try {
+        const tasks = await TasksDb.getTasks();
+        res.json(tasks.map(presentTask));
+    } catch (error) {
+        res.status(500).json({ code: 500, message: `Failed to fetch tasks. ${error}` });
+    }
 });
 
 router.get('/tasks/:id.json', async (req: express.Request, res: express.Response<ApiTask | ApiError>) => {
-    // const task = tasks.find(x => x.id === req.params.id);
     const task = await TasksDb.getTask(req.params.id);
 
     if (task) {
