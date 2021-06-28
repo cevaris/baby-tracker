9b4b3af94f9932d96b987bd68f6ad27e6a5644ad
Adam Cardenas <cevaris@gmail.com>
2021-06-28 12:51:20 -0600

    refactor sign up

--- frontend/src/app/components/sign-in/sign-in.component.ts
+++ frontend/src/app/components/sign-in/sign-in.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+
+type FormData = {
+  userName: string
+  userPassword: string
+}
 
 @Component({
   selector: 'app-sign-in',
@@ -8,9 +14,31 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class SignInComponent implements OnInit {
 
-  constructor(public authService: AuthService) { }
+  form: FormGroup;
+  formError: string;
+  submitting: boolean;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      'userName': new FormControl('', [Validators.required, Validators.email]),
+      'userPassword': new FormControl('', [Validators.required])
+    });
   }
 
+  async onSubmit() {
+    this.submitting = true;
+
+    try {
+      const data: FormData = this.form.value
+      await this.authService.signIn(data.userName, data.userPassword);
+    } finally {
+      this.submitting = false;
+    }
+  }
+
+  async googleAuth() {
+    this.authService.googleAuth();
+  }
 }
